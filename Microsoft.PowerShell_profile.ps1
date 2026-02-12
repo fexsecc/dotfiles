8b9be39fe970858e86e7ed5d44ade6482e1c05df
fexsecc <165517769+fexsecc@users.noreply.github.com>
2026-02-12 13:58:49 +0200

    Implement vc function for Visual C++ setup
    Add function to set up Visual C++ environment

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,18 @@ function prompt {
         "PS $p> "
     }
 }
+
+function vc {
+    $path = 'C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvarsall.bat'
+    if (-not (Test-Path $path)) { throw "vcvarsall not found: $path" }
+
+    # Run vcvarsall in cmd.exe and capture the environment it sets, then import into PowerShell
+    cmd /c "`"$path`" amd64 & set" | ForEach-Object {
+        if ($_ -match '^(.*?)=(.*)$') {
+            $name  = $matches[1]
+            $value = $matches[2]
+            Set-Item -Force -Path "Env:$name" -Value $value
+        }
+    }
+}
+
