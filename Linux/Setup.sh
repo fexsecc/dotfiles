a7572b00e6c0db97ab521bb3ae3d6fa4a8a6e845
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-06-01 16:10:03 +0300

    Start linux setup.sh

---
+++ Linux/Setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+CopyConfig() {
+    local TargetFile=$1
+    local MainPath="./${TargetFile}"
+    local DestinationDir=$2
+    local OutputFileName=${3:-$TargetFile}
+
+    /bin/mkdir -p "$DestinationDir"
+    /bin/cp "$MainPath" "${DestinationDir}/${OutputFileName}"
+    # Strip CRLF if needed
+    /bin/sed -i 's/\r$//' "${DestinationDir}/${OutputFileName}"
+}
+
+# Configure zsh
+CopyConfig "./Cli/zshrc" "$HOME" ".zshrc"
+/bin/mkdir -p "$HOME/.config/zsh/"
+/bin/touch "$HOME/.config/zsh/zsh_history"
+# Configure tmux and tpm plugins
+CopyConfig "./Cli/tmux.conf" "$HOME/.config/tmux"
+TpmDir="$HOME/.config/tmux/plugins/tpm"
+if [ ! -d "$TpmDir" ]; then
+    /bin/git clone https://github.com/tmux-plugins/tpm "$TpmDir"
+fi
+"$TpmDir/bin/install_plugins"
+"$TpmDir/bin/update_plugins" all
