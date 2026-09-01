84ab22039f1a3224f9cedd14ea329a27c7bc1344
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-01 15:26:07 +0300

    Try fix tmux

--- IOS/Setup.sh
+++ IOS/Setup.sh
@@ -1,8 +1,13 @@
-#!/var/jb/bin/zsh
+#!/var/jb/usr/bin/zsh
 
 set -eu
 # Change directory to the script's actual location
 cd "${0:A:h}"
+
+# Suppress bash locale warnings
+export LC_ALL=C
+export LANG=C
+
 
 CopyConfig() {
     local TargetFile=$1
@@ -27,5 +32,8 @@ TpmDir="$HOME/.config/tmux/plugins/tpm"
 if [ ! -d "$TpmDir" ]; then
     git clone https://github.com/tmux-plugins/tpm "$TpmDir"
 fi
+# Force TPM to recognize the custom config and plugin paths
+export TMUX_CONF="$HOME/.config/tmux/tmux.conf"
+export TMUX_PLUGIN_MANAGER_PATH="$HOME/.config/tmux/plugins"
 "$TpmDir/bin/install_plugins"
 "$TpmDir/bin/update_plugins" all
