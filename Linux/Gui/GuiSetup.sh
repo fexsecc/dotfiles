2e73edf0751376d615e27f130472be9a6e67d156
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-18 14:31:31 +0300

    Add zprofile autostart UWSM hyprland

--- Linux/Gui/GuiSetup.sh
+++ Linux/Gui/GuiSetup.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
 yay --needed -S hyprland uwsm libnewt hyprpaper hyprlock hypridle dunst
+
+# -e — exit immediately if a command fails
+# -u — treat unset variables as errors
+# -x — print commands before executing them
+set -eux
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Error: this script must not be run as root." >&2
+    exit 1
+fi
+
+# Change directory to the script's actual location
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$SCRIPT_DIR"
+
+cp -r ./hypr/ "$XDG_CONFIG_HOME/"
+cp ./zprofile "$HOME/.zprofile"
