d484ae0f9ed555270de62c3ab1b70cc585063c6c
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-20 17:05:11 +0300

    Update Linux desktop config

--- Linux/Gui/GuiSetup.sh
+++ Linux/Gui/GuiSetup.sh
@@ -4,7 +4,7 @@ yay --needed -S \
     hyprland uwsm libnewt waybar hyprpaper hyprlock hypridle \
     dunst alacritty network-manager-applet \
     pcmanfm grim slurp wl-clipboard blueman \
-    brightnessctl
+    brightnessctl darkman geoclue
 
 # -e — exit immediately if a command fails
 # -u — treat unset variables as errors
@@ -20,11 +20,13 @@ fi
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$SCRIPT_DIR"
 
+# NOTE: cannot use for root owned dirs! (e.g. /etc/conf.d)
 CopyConfig() {
     TargetFile=$1
     MainPath="./${TargetFile}"
     DestinationDir=$2
-    OutputFileName=${3:-$TargetFile}
+    # Extracts just the filename from the path if $3 is empty
+    OutputFileName=${3:-$(basename "$TargetFile")}
 
     mkdir -p "$DestinationDir"
     cp "$MainPath" "${DestinationDir}/${OutputFileName}"
@@ -32,16 +34,20 @@ CopyConfig() {
     sed -i 's/\r$//' "${DestinationDir}/${OutputFileName}"
 }
 
+# hyprland config
 cp -r ./hypr/ "$XDG_CONFIG_HOME/"
 cp ../../Assets/nasa.jpg "$XDG_CONFIG_HOME/hypr/"
 cp ../../Assets/morfeu.jpg "$XDG_CONFIG_HOME/hypr/"
 cp -r ./waybar/ "$XDG_CONFIG_HOME/"
-cp ./zprofile "$HOME/.zprofile"
-
-mkdir -p "$HOME/.config/dunst/"
-CopyConfig "./dunstrc" "$HOME/.config/dunst" "dunstrc"
-
+cp ./misc/zprofile "$HOME/.zprofile"
 systemctl --user enable hypridle.service
 systemctl --user enable hyprpaper.service
 sudo usermod -aG video,input $USER
 echo "[*] Reboot is required for groups to refresh"
+# dunst notification daemon
+CopyConfig "./misc/dunstrc" "$XDG_CONFIG_HOME/dunst" "dunstrc"
+# geoclue - D-Bus service for geolocation. Used for darkman
+sudo cp ./misc/geoclue.conf /etc/geoclue/
+# darkman - switch between light/dark mode depending on time
+CopyConfig "./misc/darkman_config.yaml" "$XDG_CONFIG_HOME/darkman" "config.yaml"
+systemctl --user enable --now darkman.service
