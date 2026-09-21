a85ffd7f3f703a0c85db0c40f4663ada7f45bcc3
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-21 19:14:50 +0300

    Update Linux GuiSetup.sh script

--- Linux/Setup.sh
+++ Linux/Setup.sh
@@ -15,7 +15,7 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$SCRIPT_DIR"
 
 # Make sure we have required packages
-sudo pacman -S --needed zsh tmux unzip nvim base-devel nodejs npm gdb
+sudo pacman -S --needed zsh tmux fontconfig unzip nvim base-devel nodejs npm gdb
 
 sudo ./SetupNerdFonts.sh
 
