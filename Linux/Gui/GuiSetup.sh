112332f1e40914e466c4413fa0e361302141b949
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-21 17:36:54 +0300

    Update Linux GUI config

--- Linux/Gui/GuiSetup.sh
+++ Linux/Gui/GuiSetup.sh
@@ -30,7 +30,8 @@ yay --needed -S \
     zathura zathura-pdf-mupdf mpv ungoogled-chromium-bin \
     pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber  \
     vesktop-bin pavucontrol pamixer otf-font-awesome gvfs-smb darkman \
-    kvantum qt5-wayland qt6-wayland papirus-icon-theme power-profiles-daemon
+    kvantum qt5-wayland qt6-wayland papirus-icon-theme power-profiles-daemon \
+    qemu-desktop libvirt virt-manager git base-devel
 
 # NOTE: cannot use for root owned dirs! (e.g. /etc/conf.d)
 CopyConfig() {
@@ -74,3 +75,6 @@ sed -i s/user123/$USER/g "$XDG_CONFIG_HOME/pcmanfm-qt/default/settings.conf"
 # file picker bookmarks
 CopyConfig "./misc/gtk-3.0/bookmarks" "$XDG_CONFIG_HOME/gtk-3.0/"
 sed -i s/user123/$USER/g "$XDG_CONFIG_HOME/gtk-3.0/bookmarks"
+# virt-manager QEMU/KVM setup
+sudo systemctl enable libvirtd
+sudo usermod -aG libvirt,kvm $USER
