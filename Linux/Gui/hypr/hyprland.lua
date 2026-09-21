e13c4c9cb0a2cb16ab14cdd95862126c4ca98de6
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-21 17:07:41 +0300

    Update hyprland config

--- Linux/Gui/hypr/hyprland.lua
+++ Linux/Gui/hypr/hyprland.lua
@@ -262,6 +262,8 @@ hl.config({
         force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
         disable_splash_rendering = true,
+        key_press_enables_dpms = true,
+        mouse_move_enables_dpms = true,
     },
 
     -- https://wiki.hypr.land/Configuring/Variables/#input
