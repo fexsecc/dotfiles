22cfa2c164675e5cef7a8aeb7a87222fb56742d3
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-22 19:08:37 +0300

    disable suspend in vms

--- Linux/VmTweaks.sh
+++ Linux/VmTweaks.sh
@@ -4,5 +4,8 @@ set -eux
 
 # Higher scale for better readability
 sed -i 's/scale = .*$/scale = 1.33,/g' "$XDG_CONFIG_HOME/hypr/hyprland.lua"
+# Use firefox by default inside vms
 sudo pacman --needed -S firefox
 sed -i 's/local browser = .*$/local browser = "firefox"/g' "$XDG_CONFIG_HOME/hypr/hyprland.lua"
+# Disable suspend
+sed -i '/systemctl suspend/d' "$XDG_CONFIG_HOME/hypr/hypridle.conf"
