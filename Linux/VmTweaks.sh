35ab5ad6fee0819f9db068f4045d39d9d0777ac1
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-22 18:35:14 +0300

    Add firefox setup to Linux VmTweaks

--- Linux/VmTweaks.sh
+++ Linux/VmTweaks.sh
@@ -4,3 +4,5 @@ set -eux
 
 # Higher scale for better readability
 sed -i 's/scale = .*$/scale = 1.33,/g' "$XDG_CONFIG_HOME/hypr/hyprland.lua"
+sudo pacman --needed -S firefox
+sed -i 's/local browser = .*$/local browser = "firefox"/g' "$XDG_CONFIG_HOME/hypr/hyprland.lua"
