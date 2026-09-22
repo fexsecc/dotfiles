4a2d12bcfcbdf5976109489fcc979df136dce93e
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-22 18:13:38 +0300

    Alacritty theme change

--- Linux/Gui/misc/darkman/scripts/change_theme.sh
+++ Linux/Gui/misc/darkman/scripts/change_theme.sh
@@ -9,6 +9,7 @@ case "$MODE" in
         GTK_THEME="Adwaita-dark"
         COLOR_SCHEME="prefer-dark"
         KV_THEME="KvArcDark"
+        NVIM_THEME="github_dark_high_contrast"
         MSG="Dark mode activated"
         ;;
     *light*)
@@ -17,6 +18,7 @@ case "$MODE" in
         GTK_THEME="Adwaita"
         COLOR_SCHEME="prefer-light"
         KV_THEME="KvArc"
+        NVIM_THEME="github_light_high_contrast"
         MSG="Light mode activated"
         ;;
     *)
@@ -32,10 +34,19 @@ gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
 # Apply Qt / Kvantum themes
 kvantummanager --set "$KV_THEME"
 
+# Apply alacritty theme
 if [ "$TARGET" = "dark" ]; then
     ln -sf ~/.config/alacritty/dark.toml ~/.config/alacritty/theme.toml
 elif [ "$TARGET" = "light" ]; then
     ln -sf ~/.config/alacritty/light.toml ~/.config/alacritty/theme.toml
 fi
+
+# Apply neovim theme
+echo "vim.cmd('colorscheme $NVIM_THEME')" > ~/.config/nvim/lua/current_theme.lua
+for server in "${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"/nvim.*; do
+    if [ -S "$server" ]; then
+        nvim --server "$server" --remote-expr "execute('colorscheme $NVIM_THEME')" >/dev/null 2>&1
+    fi
+done
 
 notify-send "Darkman theme selector" "$MSG"
